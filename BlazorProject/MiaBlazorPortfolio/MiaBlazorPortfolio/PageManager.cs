babe87bc0fa86aa35ffbb7e966ba939495a1928b
Mia Kellett <71409898+MiaKellett02@users.noreply.github.com>
2024-12-06 16:40:38 +0000

    Social links recreated, dark mode toggle, fromage fortress page content added

---
+++ BlazorProject/MiaBlazorPortfolio/MiaBlazorPortfolio/PageManager.cs
@@ -0,0 +1,17 @@
+﻿namespace MiaBlazorPortfolio
+{
+    public class PageManager
+    {
+        private string m_homeLocation = "";
+
+        public string GetHomeLocation()
+        {
+            return m_homeLocation;
+        }
+
+        public void SetHomeLocation(string location)
+        {
+            m_homeLocation = location;
+        }
+    }
+}
